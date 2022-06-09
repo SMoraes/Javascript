98e5b28c064cd1792ac399ec6254d81d97620cf4
sergio moraes <sergiomoraes.rc@gmail.com>
2022-06-09 11:37:29 -0300

    Javascript condicional

--- javascript/condicional.js
+++ javascript/condicional.js
@@ -1,9 +1,9 @@
-var hora = 15;
+var hora = 11;
 
 if (hora < 12) {
     console.log("Bom dia");
 
-}else if(hora >= 12 & hora <= 18)
+}else if(hora >= 12 && hora <= 18)
 {
     console.log("Boa tarde");
 
