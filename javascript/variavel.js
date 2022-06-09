8c3b7cf881a8cc2047b6e0c5d6668fccd200b928
sergio moraes <sergiomoraes.rc@gmail.com>
2022-06-09 13:41:04 -0300

    var let e const

--- javascript/variavel.js
+++ javascript/variavel.js
@@ -1,20 +1,47 @@
 // Variáveis (1/2)
 
-var nome = "Sergio Moraes"
-var idade = 90;
+// var nome = "Sergio Moraes"
+// var idade = 90;
 
-alert(nome);
-
-
-var x = 10;
-var y = 15;
-var total = x + y;
-
-alert(total);
+// alert(nome);
 
 
-var nome = "Sergio"; 
-var sobrenome = "Moraes";
-var nomeCompleto = nome+" "+sobrenome;
+// var x = 10;
+// var y = 15;
+// var total = x + y;
 
-alert(nomeCompleto);
+// alert(total);
+
+
+// var nome = "Sergio"; 
+// var sobrenome = "Moraes";
+// var nomeCompleto = nome+" "+sobrenome;
+
+// alert(nomeCompleto);
+
+// Variáveis (2/2)
+
+// Variavel global
+
+var nome = "Sergio";
+
+if (nome == "Sergio") {
+    var idade = 28;
+}
+console.log(idade);
+
+// Disponivel apenas em uma parte do código ou escopo.
+ var nome = "Sergio"; 
+
+if (nome == "Sergio") {
+    let idade = 28;
+   
+}
+
+console.log(idade);
+
+// Variavel de valor constante
+
+const nome = "Sergio";
+nome = "Adriano";
+console.log(nome);
