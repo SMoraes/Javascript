23a7de4fb9c00946594a302fc8c7cfb1c4235701
sergio moraes <sergiomoraes.rc@gmail.com>
2022-06-09 09:19:57 -0300

    Formas de mostrar (Output)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Opa, estou funcionando");
+// Quatro formas principais de você mostrar informações(dados) para o usuário. (Output)
+
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
+document.write("Algum Texto");
+
+window.alert("Mensagem de exemplo");
+
+// Para o programador
+console.log("A mensagem que eu inserir aqui, aparecerá no console.");
